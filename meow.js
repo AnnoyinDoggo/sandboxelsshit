22829c256c42d2f58389ded12cedb933a33a61ca
Doggo <120185981+AnnoyinDoggo@users.noreply.github.com>
2026-05-03 09:10:11 +0000

    Update heatsink element to include behavior and category

--- meow.js
+++ meow.js
@@ -1,13 +1,14 @@
 elements.heatsink = {
-	color: "#0000ff",
-	tick: function(pixel) {
-		if (pixel.temp < -258) {
-			pixel.temp = -273;
-		} else {
-			pixel.temp -= 15;
-		}
-	},
-	category: "land",
-	insulate: true,
-	state: "solid"
+    color: "#0000ff",
+    behavior: behaviors.WALL,
+    tick: function(pixel) {
+        if (pixel.temp < -258) {
+            pixel.temp = -273;
+        } else {
+            pixel.temp -= 15;
+        }
+    },
+    category: "machines",
+    insulate: true,
+    state: "solid"
 };
