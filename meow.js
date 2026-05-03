47ebaaba217b84762a15ee05895e703cbcefb0ac
Doggo <120185981+AnnoyinDoggo@users.noreply.github.com>
2026-05-03 09:26:16 +0000

    Refactor heatsink tick function to set temp directly
    Simplified the tick function for heatsink element.

--- meow.js
+++ meow.js
@@ -2,11 +2,7 @@ elements.heatsink = {
     color: "#0000ff",
     behavior: behaviors.WALL,
     tick: function(pixel) {
-        if (pixel.temp < -258) {
-            pixel.temp = -273;
-        } else {
-            pixel.temp -= 15;
-        }
+        pixel.temp = -273; 
     },
     category: "machines",
     insulate: true,
